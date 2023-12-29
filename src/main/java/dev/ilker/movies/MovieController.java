2d13444a4375dfdb991e814ed4eac26989ebae75
İlker Karaca <ikaracadeveloper@gmail.com>
2023-12-30 00:31:38 +0300

    updated

--- src/main/java/dev/ilker/movies/MovieController.java
+++ src/main/java/dev/ilker/movies/MovieController.java
@@ -3,7 +3,7 @@ package dev.ilker.movies;
 import java.util.List;
 import java.util.Optional;
 
-import org.bson.types.ObjectId;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,8 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
--- src/main/java/dev/ilker/movies/MovieService.java
+++ src/main/java/dev/ilker/movies/MovieService.java
@@ -3,7 +3,7 @@ package dev.ilker.movies;
 import java.util.List;
 import java.util.Optional;
 
-import org.bson.types.ObjectId;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 

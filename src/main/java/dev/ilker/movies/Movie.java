db5fb4ffa075ec3b0599ee56da317a23481d2d2c
İlker Karaca <ikaracadeveloper@gmail.com>
2023-12-29 18:50:33 +0300

    @Data import lombok.Data;

--- src/main/java/dev/ilker/movies/Movie.java
+++ src/main/java/dev/ilker/movies/Movie.java
@@ -1,13 +1,16 @@
 package dev.ilker.movies;
 
 import java.util.List;
-
+import lombok.Data;
 import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "movies")
+@Data
 
 public class Movie {
+  @Id
   private ObjectId id;
   private String imdbId;
   private String title;

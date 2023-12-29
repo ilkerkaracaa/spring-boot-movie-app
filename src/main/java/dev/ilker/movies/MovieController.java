25b5467cb8269ffaa089f6464880c2d752087287
İlker Karaca <ikaracadeveloper@gmail.com>
2023-12-29 20:25:28 +0300

    getting data from database

--- src/main/java/dev/ilker/movies/MovieController.java
+++ src/main/java/dev/ilker/movies/MovieController.java
@@ -1,9 +1,15 @@
 package dev.ilker.movies;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,8 +20,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 public class MovieController {
 
+  @Autowired
+  private MovieService movieService;  
+
   @GetMapping
-  public ResponseEntity<String> allMovies() {
-    return new ResponseEntity<String>("All movies", HttpStatus.OK);
+  public ResponseEntity<List<Movie>> getAllMovies() {
+    return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
   }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable ObjectId id) {
+    return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(id), HttpStatus.OK);
+   
+  }
+
 }

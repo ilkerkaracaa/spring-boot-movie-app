cdc3f5f6a8dd0ddaef70f62723d25e1383d28a0b
İlker Karaca <ikaracadeveloper@gmail.com>
2023-12-30 13:31:43 +0300

    client and api connection completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,58 @@
 import "./App.css";
+import api from "./api/axiosConfig";
 import { useState, useEffect } from "react";
 
 function App() {
+  const [movies, setMovies] = useState();
+  const getMovies = async () => {
+    try {
+      const response = await api.get("/api/v1/movies");
+      console.log(response.data);
+      setMovies(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
   return <div className="App"></div>;
 }
+
+// import "./App.css";
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
+
+// function App() {
+//   const [films, setFilms] = useState([]);
+
+//   useEffect(() => {
+//     // Endpoint URL'nizi buraya yazın
+//     const endpoint = "http://localhost:8080/api/v1/movies"; // Örnek URL
+
+//     // Axios ile veri isteği yapma
+//     axios
+//       .get(endpoint)
+//       .then((response) => {
+//         setFilms(response.data);
+//       })
+//       .catch((error) => {
+//         console.error("Error fetching data:", error);
+//       });
+//   }, []);
+
+//   console.log(films);
+
+//   return (
+//     <div>
+//       <h1>Filmler</h1>
+//       <ul>
+//         {films.map((film) => (
+//           <li key={film.imdbId}>{film.title}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
 
 export default App;
--- src/main/java/dev/ilker/movies/MovieController.java
+++ src/main/java/dev/ilker/movies/MovieController.java
@@ -8,11 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/movies")
 

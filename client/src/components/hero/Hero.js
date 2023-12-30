65365b6ca4c8ea0e177133757dbc33ccf8113281
İlker Karaca <ikaracadeveloper@gmail.com>
2023-12-30 20:34:29 +0300

    client Design

--- client/src/components/hero/Hero.js
+++ client/src/components/hero/Hero.js
@@ -1,6 +1,8 @@
 import "./Hero.css";
 import Carousel from "react-material-ui-carousel";
 import { Paper } from "@mui/material";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
 
 const Hero = ({ movies }) => {
   if (!movies || !Array.isArray(movies)) return <div>Loading movies...</div>;
@@ -11,13 +13,24 @@ const Hero = ({ movies }) => {
           return (
             <Paper>
               <div className="movie-card-container">
-                <div className="movie-card">
+                <div
+                  className="movie-card"
+                  style={{ "--img": `url(${movie.backdrops[0]})` }}
+                >
                   <div className="movie-detail">
                     <div className="movie-poster">
                       <img src={movie.poster} alt="" />
                     </div>
                     <div className="movie-title">
                       <h3>{movie.title}</h3>
+                    </div>
+                    <div className="movie-buttons-container">
+                      <div className="play-button-icon-container">
+                        <FontAwesomeIcon
+                          className="play-button-icon"
+                          icon={faCirclePlay}
+                        />
+                      </div>
                     </div>
                   </div>
                 </div>

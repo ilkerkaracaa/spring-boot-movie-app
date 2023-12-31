9f84a82d64825062108c211257e74af475fecb13
İlker Karaca <ikaracadeveloper@gmail.com>
2023-12-31 03:50:13 +0300

    completed

--- src/main/java/dev/ilker/movies/MovieRepository.java
+++ src/main/java/dev/ilker/movies/MovieRepository.java
@@ -8,5 +8,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
-  Optional<Movie> findMovieByImdbId(String imdbI);
+  Optional<Movie> findMovieByImdbId(String imdbId);
 }
